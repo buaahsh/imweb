b4d37b16af1ea93a9e8c25e56a3c5e3fb354bde7
Shaohan Huang <buaahsh@foxmail.com>
2015-11-16 01:04:59 +0800

    add tree node class

---
+++ src/cn/edu/buaa/im/model/TreeNode.java
@@ -0,0 +1,14 @@
+package cn.edu.buaa.im.model;
+
+public class TreeNode {
+	public String id;
+	public String parent;
+	public String text;
+	public String icon;
+	
+	public A_attr a_attr;
+	
+	public class A_attr{
+		public String href;
+	}
+}
