76a46e9189211a2eb99d32b6328803e12847299e
buaahsh <buaahsh@foxmail.com>
2015-12-08 20:24:34 +0800

    add view

--- WebContent/js/jstree.js
+++ WebContent/js/jstree.js
@@ -1,3 +1,8 @@
+function CreateView(){
+	$("#select_view").html(
+		"<option>视图</option>"
+		+ "<option>视图</option>");
+}
 function CreateTree(sid) {
 	$.getJSON("/imweb/TreeNode?sid=" + sid, function(data){
 		$('#tree')
@@ -20,6 +25,7 @@ function CreateTree(sid) {
 	            location.href= data.node.a_attr.href
 //				$("#page-content-wrapper").scrollTo(document.getElementById(data.node.a_attr.href), 800);
 	        });
+		CreateView();
 		//window.location.hash = "index_pos";
 	});
 }
